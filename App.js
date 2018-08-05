5d558f7c6785bdf3262a28939cffbd64f6c3eee3
ady.limmo <ady.limmo@gmail.com>
2018-08-05 19:56:32 +0700

    penambahan Radius Absensi

--- App.js
+++ App.js
@@ -9,11 +9,21 @@ Alert,
 TouchableOpacity,
 ActivityIndicator,
 ListView,
+Dimensions,
 Button
 } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { StackNavigator } from 'react-navigation'
+import MapView, { MAP_TYPES, Polygon, ProviderPropType } from 'react-native-maps';
 
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
 class InputUsers extends Component {
   static navigationOptions = {
     title: 'Input Users'
@@ -62,6 +72,9 @@ class InputUsers extends Component {
     this.props.navigation.navigate('Four')
   }
 
+  RadiusAbsensi = () => {
+    this.props.navigation.navigate('Five')
+  }
   render() {
     return (
       <View style = {styles.Container}>
@@ -91,6 +104,9 @@ class InputUsers extends Component {
         </TouchableOpacity>
         <TouchableOpacity activeopacity = {.4} style = {styles.TouchableOpacityStyle2} onPress={this.ScanCodeQr}>
         <Text style = {styles.TextStyle}>SCAN QR</Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeopacity = {.4} style = {styles.TouchableOpacityStyle2} onPress={this.RadiusAbsensi}>
+        <Text style = {styles.TextStyle}>Radius Absensi</Text>
         </TouchableOpacity>
       </View>
     );
@@ -172,6 +188,168 @@ class ViewDataUser extends Component {
       />
       </View>
     )
+  }
+}
+
+class PolygonCreator extends Component {
+  static navigationOptions = {
+    title: 'Radius'
+  }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      polygons: [],
+      editing: null,
+      creatingHole: false,
+    };
+  }
+
+  finish() {
+    const { polygons, editing } = this.state;
+    this.setState({
+      polygons: [...polygons, editing],
+      editing: null,
+      creatingHole: false,
+    });
+  }
+
+  createHole() {
+    const { editing, creatingHole } = this.state;
+    if (!creatingHole) {
+      this.setState({
+        creatingHole: true,
+        editing: {
+          ...editing,
+          holes: [
+            ...editing.holes,
+            [],
+          ],
+        },
+      });
+    } else {
+      const holes = [...editing.holes];
+      if (holes[holes.length - 1].length === 0) {
+        holes.pop();
+        this.setState({
+          editing: {
+            ...editing,
+            holes,
+          },
+        });
+      }
+      this.setState({ creatingHole: false });
+    }
+  }
+
+  onPress(e) {
+    const { editing, creatingHole } = this.state;
+    if (!editing) {
+      this.setState({
+        editing: {
+          id: id++,
+          coordinates: [e.nativeEvent.coordinate],
+          holes: [],
+        },
+      });
+    } else if (!creatingHole) {
+      this.setState({
+        editing: {
+          ...editing,
+          coordinates: [
+            ...editing.coordinates,
+            e.nativeEvent.coordinate,
+          ],
+        },
+      });
+    } else {
+      const holes = [...editing.holes];
+      holes[holes.length - 1] = [
+        ...holes[holes.length - 1],
+        e.nativeEvent.coordinate,
+      ];
+      this.setState({
+        editing: {
+          ...editing,
+          id: id++, // keep incrementing id to trigger display refresh
+          coordinates: [
+            ...editing.coordinates,
+          ],
+          holes,
+        },
+      });
+    }
+  }
+
+  render() {
+    const mapOptions = {
+      scrollEnabled: true,
+    };
+
+    if (this.state.editing) {
+      mapOptions.scrollEnabled = false;
+      mapOptions.onPanDrag = e => this.onPress(e);
+    }
+    return(
+      <View style={styles.container}>
+        <MapView 
+        provider="google"
+        showsUserLocation={true}
+        followsUserLocation={true}
+          provider={this.props.provider}
+          style={styles.map}
+          mapType={MAP_TYPES.HYBRID}
+          initialRegion={this.state.region}
+          onPress={e => this.onPress(e)}
+          {...mapOptions}
+        >
+          {this.state.polygons.map(polygon => (
+            <Polygon
+              key={polygon.id}
+              coordinates={polygon.coordinates}
+              holes={polygon.holes}
+              strokeColor="#F00"
+              fillColor="rgba(255,0,0,0.5)"
+              strokeWidth={1}
+            />
+          ))}
+          {this.state.editing && (
+            <Polygon
+              key={this.state.editing.id}
+              coordinates={this.state.editing.coordinates}
+              holes={this.state.editing.holes}
+              strokeColor="#000"
+              fillColor="rgba(255,0,0,0.5)"
+              strokeWidth={1}
+            />
+          )}
+        </MapView>
+        <View style={styles.buttonContainer}>
+          {this.state.editing && (
+            <TouchableOpacity
+              onPress={() => this.createHole()}
+              style={[styles.bubble, styles.button]}
+            >
+              <Text>{this.state.creatingHole ? 'Finish Hole' : 'Create Hole'}</Text>
+            </TouchableOpacity>
+          )}
+          {this.state.editing && (
+            <TouchableOpacity
+              onPress={() => this.finish()}
+              style={[styles.bubble, styles.button]}
+            >
+              <Text>Finish</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+    );
   }
 }
 
@@ -339,8 +517,13 @@ export default App = StackNavigator ({
  First: { screen: InputUsers },
  Second: { screen: ViewDataUser },
  Three : { screen: UpdateAndDeleteUser },
- Four : { screen: ScanAbsensiQr }
+ Four : { screen: ScanAbsensiQr },
+ Five : { screen: PolygonCreator }
 });
+
+PolygonCreator.propTypes = {
+  provider: ProviderPropType,
+};
 
 const styles = StyleSheet.create({
   Container:{
@@ -411,5 +594,34 @@ const styles = StyleSheet.create({
   centerText: {
     fontSize: 12,
     color: '#777',
-  }
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
 })
